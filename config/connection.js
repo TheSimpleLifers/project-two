ff58503bf51fbb1f29f6b18e18d10935ac799b82
YimmyC <jim.cassano1@gmail.com>
2023-02-15 22:02:58 -0600

    jaws fixing

--- config/connection.js
+++ config/connection.js
@@ -32,7 +32,7 @@ const sequelize = process.env.JAWSDB_URL
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
-        host: 'localhost',
+        host: 'g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         dialect: 'mysql',
         port: 3306,
       }
